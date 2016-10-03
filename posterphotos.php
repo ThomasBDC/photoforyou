70e819b30d61e8ceb67be1a72fcf18838744d3ca
BUREAUDU <BUREAUDU@s155p07.bts.org>
2016-10-03 17:01:33 +0200

    1er commit avant le push sur github

--- posterphotos.php
+++ posterphotos.php
@@ -1,8 +1,40 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>sworm - Free CSS Template by ZyPOP</title>
+<link rel="stylesheet" href="css/styles.css" type="text/css" />
+<!--
+sworm, a free CSS web template by ZyPOP (zypopwebtemplates.com/)
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Download: http://zypopwebtemplates.com/
 
+License: Creative Commons Attribution
+//-->
+</head>
+<body>
+    <?php 
+        include("include/header.inc.php");
+        include("include/cotedroit.inc.php");
+    ?>  
+
+    <div id="body">
+		<div id="content">
+                    
+                  <form action="formulaire/sendphotos.php" method="post" enctype="multipart/form-data">
+                        Select image to upload:
+                        <input type="file" name="fileToUpload" id="fileToUpload">
+                        <input type="submit" value="Upload Image" name="submit">
+                    </form>
+
+                </div>
+       
+    	<div class="clear"></div>
+    </div>
+    <?php 
+        include("include/footer.inc.php");
+    ?>  
+
+</div>
+</body>
+</html>
